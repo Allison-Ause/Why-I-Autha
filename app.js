fd7f1130554ebb4766992e5b2dc3b262c7699306
Allison Ause <allison.ause@gmail.com>
2022-06-27 14:25:38 -0700

    fully functional, started with login ended with signout wiring

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
 // import component creators
 import createAuthForm from './components/AuthForm.js';
 import createAuthError from './components/AuthError.js';
+import { signIn, signUp } from './services/members-service.js';
 
 let errorMessage = '';
 
@@ -18,35 +19,39 @@ async function handlePageLoad() {
 
 async function handleSignIn(email, password) {
     // *** remove next line after verifying credentials are working
-    console.log(email, password);
+    // console.log(email, password);
 
-    const response = null; // *** ? (don't forget call is asynchronous!)
+    const response = await signIn(email, password); // *** ? (don't forget call is asynchronous!)
+
     checkAuth(response);
 }
 
 async function handleSignUp(email, password) {
     // *** remove next line after verifying credentials are working
-    console.log(email, password);
+    // console.log(email, password);
 
-    const response = null; // *** ? (don't forget call is asynchronous!)
+    const response = await signUp(email, password); // *** ? (don't forget call is asynchronous!)
     checkAuth(response);
 }
 
 function checkAuth(response) {
     // *** remove next line after verifying user is being returned
-    console.log(response.user);
+    // console.log(response.user); - successfully returning user object w/data
 
     if (response?.error) {
         // *** 
         // 1. console.log the response.error
+        console.log(response.error);
         // 2. set the errorMessage state from response.error.message
+        errorMessage = response.error.message;
         // (keep this line👇 before console.log)
         // eslint-disable-next-line no-console
-
+        console.log(errorMessage); // logs 'Invalid Login Credentials'
         display();
     }
     else {
         // *** redirect (use replace) to './members'
+        location.replace('./members');
     }
 }
 
--- members/members.js
+++ members/members.js
@@ -1,4 +1,6 @@
 // import services and utilities
+import { getUser, signOut } from '../services/members-service.js';
+
 // *** import needed service methods
 
 let user = null;
@@ -8,18 +10,24 @@ import createSignOut from '../components/SignOut.js';
 // write handler functions
 async function handlePageLoad() {
     // *** get the user
-
+    user = await getUser();
     // *** if there is a **not** user, redirect (use replace) to '../'
+    if (!user) {
+        location.replace('../');
+        return;
+    }
 
     display();
 }
 
 async function handleSignOut() {
     // *** call sign out (don't forget call is asynchronous!)
+    await signOut();
 }
 
 // Create each component: 
 const SignOut = createSignOut(document.querySelector('#sign-out'), { handleSignOut });
+
 
 // Roll-up display function that renders (calls with state) each component
 function display() {
